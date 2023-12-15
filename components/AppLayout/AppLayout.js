cf63175500c1bd6aad7b30c616b25bbf05fc6cf9
kcabhish <kcabhish@gmail.com>
2023-12-14 23:23:37 -0500

    chore: adding getLayout functionality

---
+++ components/AppLayout/AppLayout.js
@@ -0,0 +1,8 @@
+export const AppLayout = ({children}) => {
+    return (
+        <div>
+            This is the app AppLayout
+            <div>{children}</div>
+        </div>
+    )
+}
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,9 +1,14 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { AppLayout } from "../../components/AppLayout";
 
 export default function NewPost() {
   return (
     <div>This is the new page</div>
   )
+}
+
+NewPost.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>
 }
 
 /**
