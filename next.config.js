e7ea1aecf1822832f4a4fda2c9fc22b5100240c4
kcabhish <kcabhish@gmail.com>
2023-12-14 22:51:34 -0500

    chore: whitelisting image servers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com',
+      's.gravatar.com',
+      'avatars.githubusercontent.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
