0c7b0a520d83e8958633934557905d76a49b9cf7
kcabhish <kcabhish@gmail.com>
2023-12-14 22:54:02 -0500

    feat: authentication using oAuth

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import Link from 'next/link';
+import {useUser} from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
+
 export default function Home() {
+  const { user } = useUser();
+  console.log(user);
   return (
     <div>
-      <div>home</div>
+      <div>Home</div>
       <div>
-        <Link href='/api/auth/login'>Login</Link>
+        {
+          !!user ? (
+            <>
+              <div>
+                <Image src={user.picture} alt={user.name} height={50} width={50}/>
+              </div>
+              <div>
+                {user.email}
+              </div>
+              <Link href="/api/auth/logout">Logout</Link>
+            </>
+          ) : <Link href='/api/auth/login'>Login</Link>
+        }
       </div>
     </div>
   )
