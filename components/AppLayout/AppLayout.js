d9191285be0ed00e74f85a61166576526b850fb5
kcabhish <kcabhish@gmail.com>
2023-12-18 23:23:46 -0500

    chore: adding google fonts with logo

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -3,6 +3,7 @@ import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image"
 import Link from "next/link"
+import { Logo } from "../Logo";
 
 export const AppLayout = ({children}) => {
     const { user } = useUser();
@@ -10,7 +11,7 @@ export const AppLayout = ({children}) => {
         <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
             <div className="flex flex-col text-white overflow-hidden">
                 <div className="bg-slate-800 px-2">
-                    <div>Logo</div>
+                    <Logo />
                     <Link href="/post/new" className="bg-green-500 tracking-wider text-center text-white cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block">New Post</Link>
                     <Link href="/token-topup" className="block mt-2 text-center">
                         <FontAwesomeIcon icon={faCoins} className="text-yellow-500"/>
